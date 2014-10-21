aedc6df6efd571871d49e096d590d7fb1d63a4b7
Markus Robinson <markus.robinson@gmail.com>
2014-10-20 23:25:46 -0400

    remove old stuff

--- test.php
+++ test.php
@@ -2,15 +2,20 @@
 
 namespace Theology\Theology;
 
-/**
- * A terribly useful class
- *
- * See: http://knpuniversity.com/screencast/question-answer-day
- */
-class HalloThere
-{
-    public function sayHallo()
-    {
-        return 'Hallo!';
+function my_login_logo() { ?>
+<style type="text/css">
+    body.login div#login h1 a {
+        background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/img/theology-logo.png);
+        padding-bottom: 30px;
     }
+</style>
+<?php }
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
+
+
+
+function my_login_stylesheet() {
+    wp_enqueue_style( 'custom-login', get_template_directory_uri() . '/assets/css/login.css' );
+    wp_enqueue_script( 'custom-login', get_template_directory_uri() . '/assets/js/login.js' );
 }
+add_action( 'login_enqueue_scripts', 'my_login_stylesheet' );
